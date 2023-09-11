03e885113afc0a0befb595b3f4168ec744a1fc7a
Justin2341 <144463807+Justin2341@users.noreply.github.com>
2023-09-11 10:11:27 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.print("Hello");
-    System.out.println(" world!"); 
+public class Main
+{
+  public static void main(String[] args)
+  {
+    int day;
+    day = 4;
+    System.out.print("Days until next week ");
+    System.out.println(day);
+    
+    double money;
+    money = 35.4;
+    System.out.println("The amount is " + money);
+    
+    boolean hungry;
+    hungry = false;
+    System.out.println("Am I Hungry? " + hungry);
+    
+    String name = "Justin";
+    System.out.println("My name is : " + name);
   }
 }
