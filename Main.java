3febb80e3d2465f5a1ba611e4200bcf63af1c657
Justin2341 <144463807+Justin2341@users.noreply.github.com>
2023-10-03 10:15:59 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,13 +8,13 @@ public class Main
     System.out.println("Welcome to the supermarket!" );
     System.out.println("What would you like to buy?");
     f.Foodlist();
+    f.Foodlist(5, 3.50);
     
     int apples = 5;
     double money = 20.0;
     double appleprice = 3.5;
     boolean bought = false;
       
-    System.out.println("We have " + apples + " apples. They each cost $3.50. Would you like to buy one?");
     Scanner decision = new Scanner(System.in);
     String decision1 = decision.nextLine();
     System.out.println("You have said " + decision1 + ". Let's buy one apple.");
@@ -25,15 +25,14 @@ public class Main
     System.out.println("There are now " + apples + " apples");
     
     int bananas = 10;
-    int ppp = 2; // price per pound
-    double ppp1 = Double.valueOf(ppp); // temporarily converts to a double
+    double bananaprice = 2.0;
       
-    System.out.println("We also have " + bananas + " pounds of bananas. It is $2 per pound. Would you like to buy a pound?");
+    f.Foodlist(10, 2.0, "I love bananas!");
     String decision2 = decision.nextLine();
-    System.out.println("You have said " + decision2 + ". Let's buy a pound of bananas.");
-    money = money - ppp1;
+    System.out.println("You have said " + decision2 + ". Let's buy a banana.");
+    money = money - bananaprice;
     System.out.println("You now have $" + money);
     bananas -= 1;
-    System.out.println("There are now " + bananas + " pounds of bananas");
+    System.out.println("There are now " + bananas + " bananas");
   }
 }
