0853da44cf638682c7e5f8347b04b89da524d581
Justin2341 <144463807+Justin2341@users.noreply.github.com>
2023-10-11 09:49:08 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,25 +12,23 @@ public class Main
     
     int apples = 5;
     double money = 20.0;
-    double appleprice = 3.5;
     boolean bought = false;
       
     Scanner decision = new Scanner(System.in);
     String decision1 = decision.nextLine();
     System.out.println("You have said " + decision1 + ". Let's buy one apple.");
     bought = true;
-    money = money - appleprice;
+    money = money - f.getfood1price();
     System.out.println("You now have $" + money);
     apples -= 1;
     System.out.println("There are now " + apples + " apples");
     
     int bananas = 10;
-    double bananaprice = 2.0;
       
     f.Foodlist(10, 2.0, "I love bananas!");
     String decision2 = decision.nextLine();
     System.out.println("You have said " + decision2 + ". Let's buy a banana.");
-    money = money - bananaprice;
+    money = money - f.getfood2price();
     System.out.println("You now have $" + money);
     bananas -= 1;
     System.out.println("There are now " + bananas + " bananas");
